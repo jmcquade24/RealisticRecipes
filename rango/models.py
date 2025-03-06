326d0263d58567af266432d217ece9c12d4bd3d8
“Guanxi <157113393+GuanxiLiu123@users.noreply.github.com>
2025-03-06 14:51:59 +0000

    Views and URLs

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
+class Recipe(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name="liked_recipes", blank=True)
+    favorites = models.ManyToManyField(User, related_name="favorited_recipes", blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.recipe.title} ({self.rating}/5)"
