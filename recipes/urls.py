2824fa1809fbc29c127bb02b82968eaa405b2cf8
Yesulen Tsedensodnom <sodesulen@gmail.com>
2025-03-06 17:27:06 +0000

    changed name "rango" to "recipes"

---
+++ recipes/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from recipes import views
+
+app_name = 'recipes'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'), 
+    path('category/<slug:category_name_slug>/', views.show_category, name='show_category'),  
+    path('add_category/', views.add_category, name='add_category'),  
+    path('category/<slug:category_name_slug>/add_page/', views.add_page, name='add_page'),  
+    path('register/', views.register, name='register'),  
+    path('login/', views.user_login, name='login'),  
+    path('logout/', views.user_logout, name='logout'), 
+    path('restricted/', views.restricted, name='restricted'),
+]
