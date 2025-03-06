d13cdbf8d4a88937c757a5072864cdfde8a4fdc9
“Guanxi <157113393+GuanxiLiu123@users.noreply.github.com>
2025-03-06 14:04:14 +0000

    Merge branch 'main' of https://github.com/jmcquade24/RealisticRecipes

--- RealisticRecipes/urls.py
+++ RealisticRecipes/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
     path('', include('rango.urls')),
 ]
