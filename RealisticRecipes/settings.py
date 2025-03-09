32ad80e50b8e0df54846b6917ed8be48d78b6839
Jodie McQuade <2658629m@student.gla.ac.uk>
2025-03-09 21:11:17 +0000

    Stop tracking .DS_Store and add to .gitignore

--- RealisticRecipes/settings.py
+++ RealisticRecipes/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+<<<<<<< HEAD
     'recipes',
+=======
+    'rango'
+>>>>>>> 41ec93a ( templates and css)
 ]
 
 MIDDLEWARE = [
