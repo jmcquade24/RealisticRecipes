06a600e50998b631e9894b20a65e097076fc57ad
Yesulen Tsedensodnom <2960733t@student.gla.ac.uk>
2025-03-11 22:30:26 +0000

    fixed the urls.py and settings.py so that login page works.

--- RealisticRecipes/urls.py
+++ RealisticRecipes/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from recipes import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -27,8 +28,8 @@ urlpatterns = [
     path('recipes/', include('recipes.urls')),
 
     # User Authentication
-    path('signup', views.register, name='singup'),
-    path('login', views.user_login, name='login'),
-    path('logout', views.user_logout, name='logout'),
+    path('signup', views.register, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='recipes/login.html'), name='login'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
     path('delete_account', views.delete_account, name='delete_account'),
 ]
