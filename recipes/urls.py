8288dd16edb21a2f89e419181a09e580f70acd67
Jodie McQuade <2658629m@student.gla.ac.uk>
2025-03-26 10:59:04 +0000

    forgot password link fixed to send email

--- recipes/urls.py
+++ recipes/urls.py
@@ -62,8 +62,10 @@ urlpatterns = [
     path("accounts/delete/", views.delete_account, name="delete_account"),
 
     # Password Management
-    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
-    path('password_change_done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done')
+    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 
 
